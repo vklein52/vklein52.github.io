61b3f6714e04bf98ff8b23846dceb58d93f1a308
Vijay Klein <uytre7907@gmail.com>
2021-03-02 18:11:22 -0600

    temp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import cat from './cat.gif';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import {useSpring, animated} from 'react-spring';
+import {Spring} from 'react-spring/renderprops';
 
 function App() {
+    // const props = useSpring({opacity: 1, from: {opacity: 0}});
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={cat}/>
-                <p>
-                    I'll put a website up here eventually
-                </p>
-                <p>
-                    Meep!
-                </p>
-            </header>
+          <header className="App-header">
+            <Spring
+              from={{ opacity: 0, color: 'red' }}
+              to={{ opacity: 1 , color: 'blue' }}>
+              {props => <div style={props}>hello</div>}
+            </Spring>
+            <img src={cat}/>
+            <div class="mdc-card mdc-card--outlined">
+              <p>
+                I'll put a website up here eventually
+              </p>
+              <p>
+                Meep!
+              </p>
+              <div class="mdc-card__actions">
+                <button class="mdc-button mdc-card__action mdc-card__action--button">
+                  <div class="mdc-button__ripple"></div>
+                  <span class="mdc-button__label">Action 1</span>
+                </button>
+                <button class="mdc-button mdc-card__action mdc-card__action--button">
+                  <div class="mdc-button__ripple"></div>
+                  <span class="mdc-button__label">Action 2</span>
+                </button>
+              </div>
+            </div>
+            <Button class="mdc-button mdc-button--raised">
+              <div class="mdc-button__ripple"></div>
+              <i class="material-icons mdc-button__icon" aria-hidden="true"
+              >bookmark</i
+              >
+              <span class="mdc-button__label">Contained Button plus Icon</span>
+            </Button>
+          </header>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+    </ThemeProvider>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
